c7931a93e60a1ae731c92b03aaa44aa762b5413d
DavidGanfornina <73431730+DavidGanfornina@users.noreply.github.com>
2020-12-15 21:55:38 +0100

    Update Final.js

--- BattleWeen/scenes/Final.js
+++ BattleWeen/scenes/Final.js
@@ -14,6 +14,7 @@ export class Final extends Phaser.Scene {
     this.load.image('Zombie', 'assets/images/Zombie.png');
     this.load.image('p1V', 'assets/images/p1Victory.png');
     this.load.image('p2V', 'assets/images/p2Victory.png');
+    this.load.image('fondito','assets/images/pointsBg.png')
 
     this.botonAgain.preload();
     this.load.audio('intro','assets/sounds/Music.mp3' )
@@ -27,20 +28,26 @@ export class Final extends Phaser.Scene {
     this.background= this.add.image(410, 280, 'background');
     this.background.setScale(2);
     if(this.Punt>this.Punt2){
-      this.add.image(200,80, 'p1V');
       this.Bruja= this.add.image(410, 300, 'Bruja');
+      this.add.image(400,95, 'p1V').setScale(2);
       this.Bruja.setScale(0.7);
       //this.add.text(200,80, 'HA GANADO EL PLAYER 1',{ fontSize: '35px', fill: '#000', backgroundColor: 'white', })
 
     }
     else{
-      this.add.image(200,80, 'p2V');
       this.Zombie= this.add.image(410, 300, 'Zombie');
+      this.add.image(400,95, 'p2V').setScale(2);
+      
       this.Zombie.setScale(0.7);
       //this.add.text(200,80, 'HA GANADO EL PLAYER 2',{ fontSize: '35px', fill: '#000', backgroundColor: 'white', })
     }
-    this.NumeroVida= this.add.text(50,5,'Victorias P1: ' + this.Punt, { fontSize: '32px', fill: '#000', backgroundColor: 'pink', });
-    this.NumeroVida2= this.add.text(500,5,'Victorias P2: ' + this.Punt2, { fontSize: '32px', fill: '#000', backgroundColor: 'powderblue', }); 
+
+    this.add.image(200,5 ,'fondito').setScale(1.8,1);
+    this.add.image(650,5 ,'fondito').setScale(1.8,1);
+
+    this.NumeroVida= this.add.text(50,5,'Victorias P1: ' + this.Punt, { fontSize: '32px', fill: 'white', fontStyle:'bold' });
+    this.NumeroVida2= this.add.text(500,5,'Victorias P2: ' + this.Punt2, { fontSize: '32px', fill: 'white', fontStyle:'bold'}); 
+
 
     
     this.botonAgain.create();
