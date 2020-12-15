e8072771639daf5ea4808e2ef8d959cb1bafa779
Darsay <62678776+darsay@users.noreply.github.com>
2020-12-15 12:06:53 +0100

    Botones genéricos y menú

--- BattleWeen/scenes/menu.js
+++ BattleWeen/scenes/menu.js
@@ -3,23 +3,27 @@ import { boton } from "../components/boton.js";
 export class Menu extends Phaser.Scene {
   constructor() {
     super({ key: 'menu' });
-    this.botonPlay = new boton(this, 'game');
+    this.botonPlay = new boton(this, 'game', 'button', 400, 400);
+    this.botonTutorial = new boton(this, 'game', 'tutorialBoton', 400, 500);
   }
 
   preload() {
     this.load.image('logo', 'assets/images/logo.png');
     this.load.image('background', 'assets/images/fondo.jpg');
     this.botonPlay.preload();
+    this.botonTutorial.preload();
     this.load.audio('intro','assets/sounds/DespacitoMal.mp3' )
+    this.load.image('button', 'assets/images/play.png');
+    this.load.image('tutorialBoton', 'assets/images/tutorial_boton.png');
   }
   
   create() {
     this.musica= this.sound.add('intro');
     this.add.image(410, 280, 'background');
     this.botonPlay.create();
+    this.botonTutorial.create();
     this.logo = this.add.image(400, 130, 'logo');
     this.logo.setScale(0.5);
     this.musica.play();
-    
-}
+  }
 }
